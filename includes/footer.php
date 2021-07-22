f9f267e4cd2f494b639e90e8a4650840cdc26899
KitanovskaMaryam <76153680+KitanovskaMaryam@users.noreply.github.com>
2021-07-22 15:23:02 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+          <p>Copyright 2021 by Semos Edu. All Rights Resived</p>
+</footer>
+<script>
+function resetFunction() {
+  document.getElementById("registerForm").reset();
+}
+</script>
+</body>
+</html>
